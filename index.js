244d32d5fc2fe9381a3a444e5d6a21e4ae16edbe
4sporus <porusmartin14@gmail.com>
2017-03-13 17:28:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -17,15 +17,42 @@ app.post('/save',function(req,res) {
 	
 	console.log("else statement");
 	console.log("req id-->"+req);
-	console.log("req id-->"+JSON.stringify(req));
 	
+	
+	
+	
+	db.collection('test').insert(req, function(err, records) {
+		if (err) throw err;
+		console.log("Record added as "+records[0]._id);
+	});
 	
 	res.send('success')
   }
   
 	});
-		
-
-	
 });
+
+
+
+
+// For saveing the data base 
+app.post('/update',function(req,res) {
+	console.log("start");
+	
+	MongoClient.connect("mongodb://test:test@ds147799.mlab.com:47799/snow_nodejs_mongo_test", function(err, db) {
+  if(!err) {
+    console.log("mongodb is connected");
+	
+	console.log("else statement");
+	console.log("req id-->"+req);
+	
+	db.collection('test').save(req); 
+	
+	res.send('success')
+  }
+  
+	});
+});
+
+
 app.listen(3000);
